ab93f294c9f474fb040bbafb4432a114f4c65013
hexf00 <hexf00@gmail.com>
2022-05-11 20:34:01 +0800

    feat:通过hack方式解决删除为空后的

--- src/views/editor/Editor/service.ts
+++ src/views/editor/Editor/service.ts
@@ -1,5 +1,6 @@
+import { Vue } from 'vue-property-decorator'
+import Callback from '../../../services/Callback'
 import { IEditor, IAtom } from './index'
-import style from './index.module.scss'
 export class EditorService implements IEditor {
 
   msg = '富文本编辑器'
@@ -30,25 +31,39 @@ export class EditorService implements IEditor {
     e.stopPropagation()
     e.preventDefault()
 
-    const childNodes = (e.target as HTMLElement).childNodes
+    // 不能存储对象，因为对象会被改变
+    const { anchorNode, anchorOffset, focusNode, focusOffset } = window.getSelection()!
 
+    // 在setTimeout中设置选取会导致光标闪烁，需要清除选区，nextTick中设置不会
+    // window.getSelection()?.removeAllRanges()
+
+    const childNodes = (e.target as HTMLElement).childNodes
     const result: IAtom[] = []
     for (let index = 0; index < childNodes.length; index++) {
       const el = childNodes[index]
       if (el.nodeType === 3/** text */) {
         if (el.textContent) {
           const last = result[result.length - 1]
-          // 如果上一个是文本，则合并
-          if (last && last.type === undefined) {
-            last.text += el.textContent
-          } else {
-            el.textContent && result.push({ text: el.textContent })
-          }
+          result.push({ text: el.textContent })
         }
       } else {
         el.textContent && result.push({ type: 'link', text: el.textContent })
       }
     }
+
+    // hack:如果删除为空会空，浏览器插入了新的dom，导致展现异常
+    if (result.length === 0) {
+      result.push({ text: ' ' })
+    }
+
     this.data = result
+
+    //渲染后恢复选区，可与setTimeout替换
+    Vue.nextTick(() => {
+      window.getSelection()?.setBaseAndExtent(
+        anchorNode!, anchorOffset, focusNode!, focusOffset
+      )
+    })
   }
+
 }
