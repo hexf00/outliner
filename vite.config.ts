250fdbc49b3c7e814a657bdce5fd861e4768800a
hexf00 <hexf00@gmail.com>
2022-06-27 21:50:28 +0800

    feat:编辑器打包

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig, UserConfigExport } from 'vite'
 import { createVuePlugin } from 'vite-plugin-vue2'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  // @ts-ignore  换成fork库后不知道为啥这里报错，临时措施是屏蔽
-  plugins: [createVuePlugin({
-    jsx: true,
-  })],
-  resolve: {
-    alias: {
-      '@/': '/src/',
+export default defineConfig(({ command, mode }) => {
+
+  const config: UserConfigExport = {
+    // @ts-ignore  换成fork库后不知道为啥这里报错，临时措施是屏蔽
+    plugins: [createVuePlugin({
+      jsx: true,
+    })],
+    resolve: {
+      alias: {
+        '@/': '/src/',
+      },
     },
-  },
+  }
+
+  if (command === 'build') {
+    config.base = '/outliner/'
+  }
+
+  return config
+
 })
